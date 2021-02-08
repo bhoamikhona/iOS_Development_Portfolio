ff6391d3eda82149fcc60f0ef0f6ea918dfa1102
Bhoami Khona <50435319+bhoamikhona@users.noreply.github.com>
2021-02-08 11:27:17 -0600

    Display Only App
    Learned:
    - How to set up a new Xcode project.
    - How to use the interface builder to design and create the appearance of my app.
    - How to find my way around Xcode.
    - How to size and arrange various UI elements.
    - How to add custom image assets to Xcode projects.
    - How to create app icons and size them for all resolutions.
    - How to run the app on the iOS simulator as well as sideload to a physical device.

---
+++ I Am Happy/I Am Happy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Happy
+//
+//  Created by Bhoami Khona on 8/17/20.
+//  Copyright © 2020 Bhoami Khona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
